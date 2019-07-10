e6fff2fec9ef14b492448356280bea69f6af93a9
saar <saar@NB-SAAR>
2019-07-10 14:43:34 +0500

    api

--- customerService/Controllers/OrdersController.cs
+++ customerService/Controllers/OrdersController.cs
@@ -63,49 +63,6 @@ namespace customerService.Controllers
 
         }
 
-        public IActionResult GetProducts(List<Object> list)
-        {
-            foreach (var obj in list)
-            {
-                return Ok(obj);
-            }
-            return Ok();
-        }
-        // PUT: api/Orders/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutOrder([FromRoute] int id, [FromBody] Order order)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != order.Order_Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(order).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!OrderExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
         // POST: api/Orders
         [HttpPost]
         public async Task<IActionResult> PostOrder([FromBody] Order order)
@@ -120,7 +77,8 @@ namespace customerService.Controllers
 
             return CreatedAtAction("GetOrder", new { id = order.Order_Id }, order);
         }
-        // GET: api/Orders/5/////////////for getting product data https://localhost:44396/api/Orders/add [3, 4]
+        // GET: api/Orders/5///////////
+        //for getting product data https://localhost:44396/api/Orders/add [3, 4]
         [HttpGet("add")]
         public async Task<IActionResult> GetProduct([FromBody] List<int> Id)
         {
@@ -184,30 +142,6 @@ namespace customerService.Controllers
         }
 
 
-        // DELETE: api/Orders/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteOrder([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var order = await _context.Orders.FindAsync(id);
-            if (order == null)
-            {
-                return NotFound();
-            }
-
-            _context.Orders.Remove(order);
-            await _context.SaveChangesAsync();
-
-            return Ok(order);
-        }
-
-        private bool OrderExists(int id)
-        {
-            return _context.Orders.Any(e => e.Order_Id == id);
-        }
+       
     }
 }
